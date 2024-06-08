e2e445e1eb422ecca939079b56ff41219c5fcc09
Alexander <alwong@ya.ru>
2024-06-08 15:36:59 +0400

    update

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'blog') }}</title>
+        <title>{{ Illuminate\Support\Facades\Route::currentRouteName() }}</title>
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/admin/app.css') }}">
