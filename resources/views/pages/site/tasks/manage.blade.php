31933bf756a571f55802a987b98ec15952ee9ddd
Alexander <alwong@ya.ru>
2024-08-26 16:55:03 +0400

    update

--- resources/views/pages/site/tasks/manage.blade.php
+++ resources/views/pages/site/tasks/manage.blade.php
@@ -21,7 +21,7 @@
                 <form class="btn-block" action="{{ route('clear-tasks') }}" method="POST">
                     @csrf
                     @method('DELETE')
-                    <button id="clear-tasks-btn" class="clear-tasks-btn" type="submit">Очистить</button> 
+                    <button id="clear-tasks-btn" class="clear-tasks-btn" type="submit">Clear</button> 
                 </form> 
             @else
                 <p class="empty-list-note">{{ __("tasks.no_tasks") }}</p>
