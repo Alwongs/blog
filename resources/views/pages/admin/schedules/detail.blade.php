daa2cf5515d7b26b5ca134d23c6a37a78f24a861
Alexander <alwong@ya.ru>
2024-10-18 14:20:20 +0400

    update

--- resources/views/pages/admin/schedules/detail.blade.php
+++ resources/views/pages/admin/schedules/detail.blade.php
@@ -26,7 +26,7 @@
                     @foreach ($row as $key => $day)
                         <div 
                             class="m-week-row-td__td m-schedule-{{ $day['work_shift'] }}"
-                            @if($day['is_today']) style="border: 4px solid red;" @endif
+                            {{-- @if($day['is_today']) style="border: 4px solid red;" @endif --}}
                         >
                             {{ $day['day'] }}
 
